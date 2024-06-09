cf6735d4a0e100bf5bebb8247527dfcef5e7ce91
Amit Kumar <asamitg971@gmail.com>
2024-06-09 21:14:08 +0530

    cors

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const connection = require("./config/config");
 const userRouter = require("./src/router/index")
+const cors = require("cors")
 const PORT = 4040;
 connection()
-
+app.use(cors)
 app.use(express.json());
 app.use("/api",userRouter)
 
