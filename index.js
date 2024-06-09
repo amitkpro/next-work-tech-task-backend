f7a5b30b93a7bee867d13842c346da618c5924c3
Amit Kumar <asamitg971@gmail.com>
2024-06-10 00:32:56 +0530

    cors

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 const connection = require("./config/config");
-const userRouter = require("./src/router/index")
-const cors = require("cors")
-const PORT = 4040;
-connection()
-app.use(cors)
+const userRouter = require("./src/router/index");
+const cors = require("cors");
+const PORT = 4041;
+
+connection();
+app.use(cors()); // Invoke cors as a function
 app.use(express.json());
-app.use("/api",userRouter)
+app.use("/api", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("working");
+});
 
 app.listen(PORT, () => {
   console.log(`server run on ${PORT}`);
